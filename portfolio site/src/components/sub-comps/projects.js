47d6d4a01b0b453e57f3083f76ccc756a3f7ce37
Shivendra <shivharsh44@gmail.com>
2024-07-29 22:34:56 +0530

    fixed the design

--- portfolio site/src/components/sub-comps/projects.js
+++ portfolio site/src/components/sub-comps/projects.js
@@ -86,13 +86,13 @@ export default function projects() {
         <div className='col-lg-6'>
           <div className='card'>
             <div className='card-body'>
-              <h5 className='card-title'>Axon</h5>
-              <h6 className='card-subtitle mb-2 text-body-secondary'><em>Jun 24 - Jul 24</em></h6>
+              <h5 className='card-title'>WebGraze</h5>
+              <h6 className='card-subtitle mb-2 text-body-secondary'><em>Jan 24 - Feb 24</em></h6>
               <p className='card-text'>
-                A light weight scalar-level autograd engine written in c/c++ & python, like micrograd along with multidimensional array manipulation library like numpy
+                A library that allows you to scrape raw text data from various sites such as Britannica.com, Wikipedia.com & could fetch YouTube videos transcripts. It's main role is to scrape raw text data for training LLM model from scratch. I used this library to generate training data for my LLM Cookbook project & others too.
               </p>
               <div className='col-lg-12 d-flex'>
-                <a className='card-link d-flex align-items-baseline' href='https://github.com/shivendrra/axon'>github
+                <a className='card-link d-flex align-items-baseline' href='https://github.com/shivendrra/web-graze'>github
                   <svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' fill='currentColor' className='bi bi-box-arrow-up-right ms-1' viewBox='0 0 16 16'>
                     <path fill-rule='evenodd' d='M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5' />
                     <path fill-rule='evenodd' d='M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z' />
@@ -105,13 +105,13 @@ export default function projects() {
         <div className='col-lg-6'>
           <div className='card'>
             <div className='card-body'>
-              <h5 className='card-title'>WebGraze</h5>
-              <h6 className='card-subtitle mb-2 text-body-secondary'><em>Jan 24 - Feb 24</em></h6>
+              <h5 className='card-title'>Axon</h5>
+              <h6 className='card-subtitle mb-2 text-body-secondary'><em>Jun 24 - Jul 24</em></h6>
               <p className='card-text'>
-                A library that allows you to scrape raw text data from various sites such as Britannica.com, Wikipedia.com & could fetch YouTube videos transcripts. It's main role is to scrape raw text data for training LLM model from scratch. I used this library to generate training data for my LLM Cookbook project & others too.
+                A light weight scalar-level autograd engine written in c/c++ & python, like micrograd along with multidimensional array manipulation library like numpy
               </p>
               <div className='col-lg-12 d-flex'>
-                <a className='card-link d-flex align-items-baseline' href='https://github.com/shivendrra/web-graze'>github
+                <a className='card-link d-flex align-items-baseline' href='https://github.com/shivendrra/axon'>github
                   <svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' fill='currentColor' className='bi bi-box-arrow-up-right ms-1' viewBox='0 0 16 16'>
                     <path fill-rule='evenodd' d='M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5' />
                     <path fill-rule='evenodd' d='M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z' />
