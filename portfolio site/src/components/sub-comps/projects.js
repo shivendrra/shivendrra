f44a356165fa105b411c3abf498681934f9a87dc
Shivendra <shivharsh44@gmail.com>
2024-08-18 00:37:56 +0530

    new update

--- portfolio site/src/components/sub-comps/projects.js
+++ portfolio site/src/components/sub-comps/projects.js
@@ -174,7 +174,7 @@ export default function projects() {
                 Part of Axon project, that has mulit-dim python tensor class wrapper over scalar autograd written in c/c++
               </p>
               <div className='col-lg-12 d-flex'>
-                <a className='card-link d-flex align-items-baseline' href='https://github.com/shivendrra/axon'>github
+                <a className='card-link d-flex align-items-baseline' href='https://github.com/shivendrra/axon-drop'>github
                   <svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' fill='currentColor' className='bi bi-box-arrow-up-right ms-1' viewBox='0 0 16 16'>
                     <path fill-rule='evenodd' d='M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5' />
                     <path fill-rule='evenodd' d='M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z' />
