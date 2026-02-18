f0e8a6b7754516d06ec6342151fab991fb1b97c8
Shivendra <shivharsh44@gmail.com>
2026-02-19 01:34:54 +0530

    small change

--- site/constants.ts
+++ site/constants.ts
@@ -3,10 +3,10 @@ import { Project, Video } from './types';
 
 export const sampleVideos: Video[] = [
   {
-    title: 'I BUILT A GPT',
+    title: 'A message',
     thumbnail: 'https://raw.githubusercontent.com/shivendrra/shivendrra/main/website/media/Thumbnail2.png',
     description: 'A video by Shivendra',
-    url: 'https://www.youtube.com/watch?v=PVpyN_2z5II'
+    url: 'https://www.youtube.com/watch?v=WuULaBwaClc'
   },
   {
     title: 'THE ARTIST',
